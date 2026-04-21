d82b409d2b43a9fb9f03a0a3b8142b730c72f161
Cecilia Curlango <curlango@uabc.edu.mx>
2026-04-21 16:48:12 -0700

    Add files via upload

--- ArchivosDeTexto/escribirPalabras.c
+++ ArchivosDeTexto/escribirPalabras.c
@@ -5,3 +5,71 @@
 //
 // Created by Cecilia Curlango on 4/21/26.
 //
+#include <stdio.h>
+#include <string.h>
+int main() {
+    FILE *apuntador;
+    char palabra[255];
+
+    // abrir archivo
+    apuntador = fopen("palabras.txt","a");
+    if (apuntador==NULL) {
+        printf("No se pudo abrir el archivo, bye\n");
+        return 1;
+    }
+    do {
+        // leer palabra
+        printf("Escribe una palabra o (fin) para terminar\n");
+        fgets(palabra,sizeof(palabra),stdin);
+        if (strcmp(palabra,"fin\n")!=0) {
+            // escribir palabra
+        fputs(palabra,apuntador);
+        }
+
+        // si es "fin" terminar
+        //palabra[strlen(palabra)-1] = '\0';
+    }while (strcmp(palabra,"fin\n")!=0);
+
+    // cerrar archivo
+    fclose(apuntador);
+
+    // mostrar todas la palabras que tiene el archivo
+    //  e indicar cuántas son
+    apuntador = fopen("palabras.txt","r");
+    if (apuntador==NULL) {
+        printf("No se pudo abrir el archivo para lectura, bye\n");
+        return 1;
+    }
+    int cantidadDePalabras = 0;
+    while (fgets(palabra,sizeof(palabra),apuntador) != 0) {
+        cantidadDePalabras++;
+        printf("%s",palabra);
+    }
+    // cerrar archivo
+    fclose(apuntador);
+    printf("El archivo tiene %d palabras\n",cantidadDePalabras);
+
+
+    apuntador = fopen("palabras.txt","r");
+    if (apuntador==NULL) {
+        printf("No se pudo abrir el archivo para lectura, bye\n");
+        return 1;
+    }
+    char palabraBuscada[256];
+    printf("Qué palabra buscas?\n");
+    fgets(palabraBuscada,sizeof(palabraBuscada),stdin);
+    bool laEncontro = false;
+    while (fgets(palabra,sizeof(palabra),apuntador) != 0) {
+        if (strcmp(palabra,palabraBuscada)==0) {
+            laEncontro = true;
+        }
+    }
+    // cerrar archivo
+    fclose(apuntador);
+    if (laEncontro == true) {
+        printf("La palabra %s si está\n",palabraBuscada);
+    } else {
+        printf("La palabra %s NO está\n",palabraBuscada);
+    }
+    return 0;
+}
